71cbc39792d7fd2ae3589f24fe286fc4314dda3c
Pedro Rodante <107422251+PedroRodante@users.noreply.github.com>
2023-10-31 20:11:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4200;
 var bodyParser = require("body-parser");
 
 // MySQL
